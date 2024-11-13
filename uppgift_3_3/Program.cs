1022c656be49843d946e4e0f053cd162871aa4e6
EliasGoransson07EEDAT23 <elgo07001@utb.vaxjo.se>
2024-11-13 12:02:02 +0100

    Add project files.

---
+++ uppgift_3_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace uppgift_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+           
+            Console.WriteLine("Hej, för att få reda på priset för hyrbilen, skriv in antal timmar nedan. (hela timmar, avrunda uppåt)");
+            int tim = int.Parse(Console.ReadLine());
+            int pris = tim * 80;
+            if (pris < 950)
+            {
+                Console.WriteLine("Hyrbilen kommer kosta " + pris + "kr.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Hyrbilen kommer kosta 950kr.");
+                Console.ReadKey();
+            }
+            
+
+        }
+    }
+}
